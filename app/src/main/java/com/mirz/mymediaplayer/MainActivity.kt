cf884772498ebb5a7f32fca0a15e79f8dc6bd378
Mirza Ukasyah <mirzauksyh@gmail.com>
2023-04-07 13:52:49 +0700

    Integrate MediaPlayer with ForegroundService

--- app/src/main/java/com/mirz/mymediaplayer/MainActivity.kt
+++ app/src/main/java/com/mirz/mymediaplayer/MainActivity.kt
@@ -1,58 +1,77 @@
 package com.mirz.mymediaplayer
 
-import android.media.AudioAttributes
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.media.MediaPlayer
+import android.os.*
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import com.mirz.mymediaplayer.databinding.ActivityMainBinding
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var mMediaPlayer: MediaPlayer? = null
     private var isReady: Boolean = false
 
+    private var mService: Messenger? = null
+    private lateinit var mBoundServiceIntent: Intent
+    private var mServiceBound = false
+
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+
+    private val mServiceConnection = object : ServiceConnection {
+        override fun onServiceDisconnected(name: ComponentName) {
+            mService = null
+            mServiceBound = false
+        }
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            mService = Messenger(service)
+            mServiceBound = true
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        init()
-            binding.btnPlay.setOnClickListener {
-                if (!isReady) {
-                    mMediaPlayer?.prepareAsync()
-                } else {
-                    if (mMediaPlayer?.isPlaying as Boolean) {
-                        mMediaPlayer?.pause()
-                    } else {
-                        mMediaPlayer?.start()
-                    }
+        mBoundServiceIntent = Intent(this, MediaService::class.java)
+        mBoundServiceIntent.action = MediaService.ACTION_CREATE
+        startService(mBoundServiceIntent)
+        bindService(mBoundServiceIntent, mServiceConnection, Context.BIND_AUTO_CREATE)
+        binding.btnPlay.setOnClickListener {
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.PLAY, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
                 }
             }
-            binding.btnStop.setOnClickListener {
-                if (mMediaPlayer?.isPlaying as Boolean || isReady) {
-                    mMediaPlayer?.stop()
-                    isReady = false
+        }
+        binding.btnStop.setOnClickListener {
+            if (mServiceBound) {
+                try {
+                    mService?.send(Message.obtain(null, MediaService.STOP, 0, 0))
+                } catch (e: RemoteException) {
+                    e.printStackTrace()
                 }
             }
+        }
     }
 
-    private fun init() {
-        mMediaPlayer = MediaPlayer()
-        val attribute = AudioAttributes.Builder()
-            .setUsage(AudioAttributes.USAGE_MEDIA)
-            .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
-            .build()
-        mMediaPlayer?.setAudioAttributes(attribute)
-        val afd = applicationContext.resources.openRawResourceFd(R.raw.guitar_background)
-        try {
-            mMediaPlayer?.setDataSource(afd.fileDescriptor, afd.startOffset, afd.length)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }
-        mMediaPlayer?.setOnPreparedListener {
-            isReady = true
-            mMediaPlayer?.start()
-        }
-        mMediaPlayer?.setOnErrorListener { _, _, _ -> false }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy: ")
+        unbindService(mServiceConnection)
+        mBoundServiceIntent.action = MediaService.ACTION_DESTROY
+        startService(mBoundServiceIntent)
     }
+
+
 }
---
+++ app/src/main/java/com/mirz/mymediaplayer/MediaPlayerCallback.kt
@@ -0,0 +1,6 @@
+package com.mirz.mymediaplayer
+
+interface MediaPlayerCallback {
+    fun onPlay()
+    fun onStop()
+}
